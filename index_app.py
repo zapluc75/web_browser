36a9f440ce66ab7f418aef5dc29443e56027abb1
zapluc75 <lucianopaz@yahoo.com.br>
2025-07-08 15:28:01 -0300

    Add files via upload

--- index_app.py
+++ index_app.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
-st.set_page_config(page_title="Central de Aplicativos", page_icon="🧰", layout="centered")
+st.set_page_config(page_title="Central de Aplicativos", page_icon="🧰", layout="wide")
 
-st.title("🧰 Central de Aplicativos")
+# Layout de colunas para os brasões
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col1:
+    st.image("Brasao fisc sem fundo.png", use_column_width=True)
+
+with col2:
+    st.markdown("<h1 style='text-align: center;'>🧰 Central de Aplicativos DER-DF</h1>", unsafe_allow_html=True)
+
+with col3:
+    st.image("Brasao sem fundo 2.png", use_column_width=True)
+
+st.markdown("---")
 
 st.markdown("Escolha uma ferramenta abaixo:")
 
+# Links para os aplicativos
 st.markdown("### 📄 [Merge de PDFs](https://pdfmerge-software001.streamlit.app/)")
 st.markdown("### 📦 [Pesos e Dimensões](https://pesosedimensoes-software002.streamlit.app/)")
 
